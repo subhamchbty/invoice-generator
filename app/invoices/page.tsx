f7f8eb71d7e056e316914565d3c3b94953262414
subhamchbty <45362908+subhamchbty@users.noreply.github.com>
2024-01-23 22:11:39 +0530

    Initial commit

---
+++ app/invoices/page.tsx
@@ -0,0 +1,5 @@
+import Link from 'next/link';
+
+export default function () {
+  return <Link href="/">Go to Main Page</Link>;
+}
---
+++ app/page.tsx
@@ -0,0 +1,5 @@
+import Link from 'next/link';
+
+export default function () {
+  return <Link href="/invoices">Go to Invoices</Link>;
+}
