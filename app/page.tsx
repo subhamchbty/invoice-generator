42be8105b687abf99a2ef923c8fda6048dcf2f9d
Subham Chakraborty <subhamchakraborty81@gmail.com>
2024-01-23 23:28:50 +0530

    Add login component and import necessary dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+'use client'
+
+import Login from '@/components/Login'
+import { Files } from '@phosphor-icons/react'
+import Link from 'next/link'
+
 export default function Page() {
-	return <div>main page</div>
+	return <Login />
 }
